d53eaac295e9e1ad7c5851a36d5dfa70b5267984
Administrator <785282608@qq.com>
2017-11-06 15:00:59 +0800

    sdfdsf

--- myProject/src/myProject/mydemo.java
+++ myProject/src/myProject/mydemo.java
@@ -4,6 +4,10 @@ public class mydemo {
 
 	public static void main(String[] args) {
 		System.out.println("这是我第一个gitweb项目");
+		say();
 	}
 
+	static void say(){
+      System.out.println("我是一个方法！！！！");		
+	}
 }
